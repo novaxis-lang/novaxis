72a44cb5a306318e2af423681e358ff2f1431fd8
Naxeion <naxeion@proton.me>
2024-04-12 18:52:13 -0400

    feat: Add a class for handling cases of invalid index provided

---
+++ src/Core/Error/InvalidIndexException.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Novaxis\Core\Error;
+
+use Novaxis\Core\Error\Exception;
+
+/**
+ * Class InvalidIndexException
+ *
+ * Exception class representing an error when an index is invalid.
+ *
+ * @package Novaxis\Core\Error
+ */
+class InvalidIndexException extends Exception {
+    /**
+     * The default error message for the invalid index exception.
+     *
+     * @var string
+     */
+    protected $message = 'Invalid index provided.';
+
+    /**
+     * InvalidIndexException constructor.
+     *
+     * @param string|null $message The custom error message for the exception (optional).
+     * @param int $line The line number where the exception occurred (optional).
+     */
+    public function __construct(?string $message = null, int $line = 0) {
+        parent::__construct($message ?? $this -> message, $line);
+    }
+}
