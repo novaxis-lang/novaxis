1a3edbc1b0453c9cac526d9614c00ca8f3dac3bb
Naxeion <naxeion@proton.me>
2024-04-13 17:52:13 -0400

    feat: Multidimensional Indexing for `List/String` datatypes

--- src/Core/Syntax/Handler/Variable/Indexing.php
+++ src/Core/Syntax/Handler/Variable/Indexing.php
@@ -4,9 +4,11 @@ namespace Novaxis\Core\Syntax\Handler\Variable;
 use Novaxis\Core\Error\InvalidIndexException;
 use Novaxis\Core\Error\IndexOutOfRangeException;
 use Novaxis\Core\Error\IndexingValueNotExistException;
+use Novaxis\Core\Error\InvalidIndexingTargetException;
+use Novaxis\Core\Error\MultidimensionalIndexingWithStringException;
 
 class Indexing {
-	public string $pattern = "/^(\s*\S{1,}\s*)(\[\s*(\S*)\s*\])$/";
+	public string $pattern = '/^(\s*(\w+|\.){1,}\s*)(\[\s*\S*\s*\]){1,}$/';
 	public array $index_declaration_patterns = [
 		"/^\d*$/", // [0] -> first element
 		"/^\d{1,}\:$/", // [0:] -> from the first element to the end
@@ -15,7 +17,8 @@ class Indexing {
 		"/^\:$/", // [:] -> all elements
 	];
 
-	public string $index_declaration_public_pattern = "/^(\-?\d*)(\:?)(\-?\d*)$/"; // Public pattern
+	public string $index_declaration_public_pattern = '/^(\-?\d*)(\:?)(\-?\d*)$/'; // Public pattern
+	public string $index_pattern = '/\[\s*(\-?\d*?\:?\-?\d*?)\s*\]/';
 
 	public ?string $input = null;
 
@@ -57,10 +60,18 @@ class Indexing {
 		return trim($matches[1]);
 	}
 
-	public function getIndex() {
+	public function getIndex(string $index) { // or `extractIndex` as a name for the function
+		$this -> valueExists(true);
+		preg_match($this -> index_pattern, $index, $matches);
+		return $matches[1];
+	}
+
+	public function getIndexes(): array {
 		$this -> valueExists(true);
 		preg_match($this -> pattern, $this -> input, $matches);
-		return trim($matches[3]);
+		$indexes = trim($matches[3]);
+		preg_match_all($this -> index_pattern, $indexes, $matches);
+		return $matches[0];
 	}
 
 	public function getSlice(mixed $value, string $datatype) {
@@ -80,86 +91,85 @@ class Indexing {
 		}
 	}
 
-	// public function getIndexes() {
-	// 	preg_match($this -> index_declaration_public_pattern, $this -> getIndex(), $matches);
-
-	// 	return array(
-	// 		"first-index" => !empty($matches[1]) ? trim($matches[1]) : '0',
-	// 		"second-index" => !empty($matches[2]) && !empty($matches[3]) ? trim($matches[3]) : null,
-	// 	);
-	// }
-
-	public function getStringSlice(string $value) {
+	public function getStringSlice(string $value): string {
 		$this -> valueExists(true);
-		preg_match($this -> index_declaration_public_pattern, $this -> getIndex(), $matches);
-		if (isset($matches[1]) && (!empty($matches[1]) || trim($matches[1]) == '0') && empty($matches[2])) {
-			if (isset($value[(int) trim($matches[1])])) {
-				return $value[(int) trim($matches[1])];
+
+		foreach ($this -> getIndexes() as $_ => $index) {
+			if (strlen($value) <= 1) {
+				throw new InvalidIndexingTargetException;
 			}
-			else {
-				throw new IndexOutOfRangeException('Index \'' . trim($matches[1]) . '\' out of range.');
+			preg_match($this -> index_declaration_public_pattern, $this -> getIndex($index), $matches);
+			if (isset($matches[1]) && (!empty($matches[1]) || trim($matches[1]) == '0') && empty($matches[2])) {
+				if (isset($value[(int) trim($matches[1])])) {
+					$value = $value[(int) trim($matches[1])];
+				}
+				else {
+					throw new IndexOutOfRangeException('Index \'' . trim($matches[1]) . '\' out of range.');
+				}
+			}
+	
+			else if (!empty($matches[2])) {
+				$firstNumber = !empty($matches[1]) ? trim($matches[1]) : '0';
+				$secondNumber = !empty($matches[2]) && !empty($matches[3]) ? trim($matches[3]) : null;
+
+				if (!isset($value[$firstNumber])) {
+					throw new IndexOutOfRangeException('Index \'' . "$firstNumber in $this->input" . '\' out of range.');
+				}
+				else if (!isset($value[$secondNumber])) {
+					throw new IndexOutOfRangeException('Index \'' . "$secondNumber in $this->input" . '\' out of range.');
+				}
+	
+				$value = substr($value, $firstNumber, $secondNumber);
 			}
 		}
 
-		else if (!empty($matches[2])) {
-			$firstNumber = !empty($matches[1]) ? trim($matches[1]) : '0';
-			$secondNumber = !empty($matches[2]) && !empty($matches[3]) ? trim($matches[3]) : null;
-
-			if (!isset($value[$firstNumber])) {
-				throw new IndexOutOfRangeException('Index \'' . "$firstNumber in $this->input" . '\' out of range.');
-			}
-			else if (!isset($value[$secondNumber])) {
-				throw new IndexOutOfRangeException('Index \'' . "$secondNumber in $this->input" . '\' out of range.');
-			}
-
-			return substr($value, $firstNumber, $secondNumber);
-		}
+		return $value;
 	}
 
-	public function getListSlice(array $value): array {
+	public function getListSlice(array $value): mixed {
 		$this -> valueExists(true);
-		print_r($this -> getIndex());
-		preg_match($this -> index_declaration_public_pattern, $this -> getIndex(), $matches);
-
-		if (isset($matches[1]) && (!empty($matches[1]) || trim($matches[1]) == '0') && empty($matches[2])) {
-			if (isset($value[trim($matches[1])])) {
-				$value = $value[trim($matches[1])];
+		foreach ($this -> getIndexes() as $_ => $index) {
+			if (!is_array($value)) {
+				throw new InvalidIndexingTargetException;
 			}
-			else if (isset($value[count($value) + trim($matches[1])])) {
-				$value = $value[count($value) + trim($matches[1])];
+			preg_match($this -> index_declaration_public_pattern, $this -> getIndex($index), $matches);
+	
+			if (isset($matches[1]) && (!empty($matches[1]) || trim($matches[1]) == '0') && empty($matches[2])) {
+				if (isset($value[trim($matches[1])])) {
+					$value = $value[trim($matches[1])];
+				}
+				else if (isset($value[count($value) + trim($matches[1])])) {
+					$value = $value[count($value) + trim($matches[1])];
+				}
+				else {
+					throw new IndexOutOfRangeException('Index \'' . trim($matches[1]) . '\' out of range.');
+				}
+			}
+	
+			else if (!empty($matches[2])) {
+				$firstNumber = !empty($matches[1]) ? trim($matches[1]) : '0';
+				$secondNumber = !empty($matches[2]) && !empty($matches[3]) ? trim($matches[3]) : null;
+	
+				if (!isset($value[$firstNumber]) and empty($value[count($value) + $firstNumber])) {
+					throw new IndexOutOfRangeException('Index \'' . "$firstNumber in $this->input" . '\' out of range.');
+				}
+				else if ($secondNumber === null) {}
+				else if (!isset($value[$secondNumber]) and (empty($value[count($value) + $secondNumber]))) {
+					throw new IndexOutOfRangeException('Index \'' . "$secondNumber in $this->input" . '\' out of range.');
+				}
+				
+				if ($secondNumber === null) {
+					$value = array_slice($value, $firstNumber);
+				}
+				else {
+					$value = array_slice($value, $firstNumber, $secondNumber - $firstNumber);
+				}
 			}
 			else {
-				throw new IndexOutOfRangeException('Index \'' . trim($matches[1]) . '\' out of range.');
+				throw new InvalidIndexException;
 			}
-
-			if (!is_array($value)) { return [ $value ]; }
-			return $value;
 		}
 
-		else if (!empty($matches[2])) {
-			$firstNumber = !empty($matches[1]) ? trim($matches[1]) : '0';
-			$secondNumber = !empty($matches[2]) && !empty($matches[3]) ? trim($matches[3]) : null;
-
-			if (!isset($value[$firstNumber]) and empty($value[count($value) + $firstNumber])) {
-				throw new IndexOutOfRangeException('Index \'' . "$firstNumber in $this->input" . '\' out of range.');
-			}
-			else if ($secondNumber === null) {}
-			else if (!isset($value[$secondNumber]) and (empty($value[count($value) + $secondNumber]))) {
-				throw new IndexOutOfRangeException('Index \'' . "$secondNumber in $this->input" . '\' out of range.');
-			}
-			
-			if ($secondNumber === null) {
-				$result = array_slice($value, $firstNumber);
-			}
-			else {
-				$result = array_slice($value, $firstNumber, $secondNumber - $firstNumber);
-				// $result = array_merge($result, [$value[end($result)]]);
-			}
-
-			return $result;
-		}
-		else {
-			throw new InvalidIndexException;
-		}
+		return $value;
 	}
 }
