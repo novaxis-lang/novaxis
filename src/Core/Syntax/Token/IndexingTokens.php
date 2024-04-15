ac58b1f4b0a5838da3c0c8e3387d42886960eaa5
Naxeion <naxeion@proton.me>
2024-04-15 07:27:19 -0400

    Update IndexingTokens.php

--- src/Core/Syntax/Token/IndexingTokens.php
+++ src/Core/Syntax/Token/IndexingTokens.php
@@ -9,11 +9,11 @@ namespace Novaxis\Core\Syntax\Token;
 trait IndexingTokens {
 	/**
 	 * Represents the opening square bracket used for indexing.
-		*/
+	*/
 	const OPEN_INDEX = '[';
 	
 	/**
 	 * Represents the closing square bracket used for indexing.
-		*/
+	*/
 	const CLOSE_INDEX = ']';
 }
