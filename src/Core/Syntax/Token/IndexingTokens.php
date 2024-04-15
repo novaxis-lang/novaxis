a7573c85b763f24b040914654d288f821980eed0
Naxeion <naxeion@proton.me>
2024-04-15 07:26:59 -0400

    refactor: PHPDoc for IndexingTokens.php

--- src/Core/Syntax/Token/IndexingTokens.php
+++ src/Core/Syntax/Token/IndexingTokens.php
@@ -1,7 +1,19 @@
 <?php
 namespace Novaxis\Core\Syntax\Token;
 
+/**
+ * Trait IndexingTokens
+ *
+ * This trait contains a set of constants related to indexing tokens.
+ */
 trait IndexingTokens {
+	/**
+	 * Represents the opening square bracket used for indexing.
+		*/
 	const OPEN_INDEX = '[';
+	
+	/**
+	 * Represents the closing square bracket used for indexing.
+		*/
 	const CLOSE_INDEX = ']';
 }
