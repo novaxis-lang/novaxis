fa5ca208b9b45bb1d6d509bf2c0904cb93ebef16
Naxeion <naxeion@proton.me>
2024-04-15 07:42:35 -0400

    refactor: PHPDoc for ImportTokens.php

--- src/Core/Syntax/Token/ImportTokens.php
+++ src/Core/Syntax/Token/ImportTokens.php
@@ -1,10 +1,34 @@
 <?php
 namespace Novaxis\Core\Syntax\Token;
 
+/**
+ * Trait ImportTokens
+ *
+ * This trait contains a set of constants related to import tokens.
+ */
 trait ImportTokens {
+	/**
+  * Represents the keyword used for importing classes.
+  */
 	const IMPORT_KEYWORD = 'import';
+	
+	/**
+  * Represents the keyword used for providing aliases to imported classes.
+  */
 	const AS_KEYWORD = 'as';
+	
+	/**
+  * Represents the default alias used when no specific alias is provided.
+  */
 	const DEFAULT_ALIAS = '?';
+	
+	/**
+  * Represents the keyword used for publicly importing classes.
+  */
 	const IMPORTING_PUBLICLY_KEYWORD = 'publicly';
+	
+	/**
+  * Represents the keyword used for privately importing classes.
+  */
 	const IMPORTING_PRIVATELY_KEYWORD = 'privately';
 }
